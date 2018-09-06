a370351304f886d2b9c8f8ddb90e1bd5329edda9
Benjamin Cutler <benjamincutler@gamesdonequick.com>
2018-09-05 21:04:41 -0600

    point submodules at experimental branch

--- urls.py
+++ urls.py
@@ -2,16 +2,22 @@ from django.conf.urls import patterns, include, url
 
 from django.contrib import admin
 
-urlpatterns = patterns('',
-    url(r'^tracker/', include('tracker.urls')),
-    url(r'^admin/lookups/', include('ajax_select.urls')),
+import tracker.urls
+from tracker.views import logout
+import ajax_select.urls
+
+urlpatterns = [
+    url(r'^tracker/', include(tracker.urls)),
+    url(r'^admin/lookups/', include(ajax_select.urls)),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^logout/', 'tracker.views.logout'),
-)
+    url(r'^logout/', logout),
+]
 
 try:
-    import imp
-    imp.find_module('tracker_ui')
-    urlpatterns += patterns('', url(r'^ui/', include('tracker_ui.urls')))
-except ImportError:
-    print("Could not locate tracker_ui module, starting without it")
+    import tracker_ui.urls
+    urlpatterns += [url(r'^ui/', include(tracker_ui.urls))]
+except ImportError as e:
+    import traceback
+    import warnings
+    print traceback.print_exc()
+    warnings.warn("Could not locate tracker_ui module, starting without it")
